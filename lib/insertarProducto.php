1113f93389f735150e862a15e88bd62bbd14bf31
cetecom <cetecom@LC08L5724>
2017-05-23 11:21:07 -0400

    Insert creado

---
+++ lib/insertarProducto.php
@@ -0,0 +1,17 @@
+<?php
+
+    $nombre = $_POST['nombre'];
+    $totalusd = $_POST['totalusd'];
+    $ano = $_POST['ano'];
+    
+    /* Consultas SQL */
+
+    $oConn = new Conexion();
+    if ($oConn->Conectar()){
+        $db = $oConn -> objconn;
+    }else{
+        return false;
+    }
+    $sql = "INSERT INTO productos(nombre, totalusd, ano) VALUES('$nombre', $totalusd, $ano);";
+    $resultado = $db->query($sql);
+    
--- lib/logtransacciones.php
+++ lib/logtransacciones.php
@@ -1,19 +1,5 @@
 <?php
 include 'Conexion.php';
 
-    $nombre = $_POST['nombre'];
-    $totalusd = $_POST['totalusd'];
-    $ano = $_POST['ano'];
-    
-    /* Consultas SQL */
 
-    $oConn = new Conexion();
-    if ($oConn->Conectar()){
-        $db = $oConn -> objconn;
-    }else{
-        return false;
-    }
-    $sql = "INSERT INTO productos(nombre, totalusd, ano) VALUES('$nombre', $totalusd, $ano);";
-    $resultado = $db->query($sql);
-    
 
