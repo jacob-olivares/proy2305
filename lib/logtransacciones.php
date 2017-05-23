b96f85654ed588277779241b3d8f6e5ee0eceff0
cetecom <cetecom@LC08L5724>
2017-05-23 11:12:21 -0400

    Creando producto

---
+++ lib/logtransacciones.php
@@ -0,0 +1,16 @@
+<?php
+    $nombre = $_POST['nombre'];
+    $totalusd = $_POST['totalusd'];
+    $ano = $_POST['ano'];
+    
+    /* Consultas SQL */
+        $oConn = new Conexion();
+        if ($oConn->Conectar()){
+            $db = $oConn -> objconn;
+        }else{
+            return false;
+        }
+    $sql = "INSERT INTO logtransacciones(nombre, totalusd, ano) VALUES('$nombre', $totalusd, $ano);";
+    $resultado = $db->query($sql);
+    
+
--- productos.php
+++ productos.php
@@ -4,6 +4,11 @@
         <title></title>
     </head>
     <body>
-        <form action=""
+        <form action="lib/logtransacciones.php" method="post">
+            <div id="ds">Nombre Producto:<input type="text" name="nombre"></div>
+            <div id="ds">Total USD:<input type="text" name="totalusd"></div>
+            <div id="ds">Año<input type="text" name="ano"></div>
+            <input type="submit" value="Registrar Producto">
+        </form>
     </body>
 </html>
